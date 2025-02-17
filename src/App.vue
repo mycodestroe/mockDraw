f9d9ba52003f91d28ed66f3c850faff598fea85e
WDL <wangdelei4406@yundasys.com>
2025-02-17 17:01:28 +0800

    抽奖

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,173 @@
+<!--
+ * @Author: wdl
+ * @Email: wangdelei4406@yundasys.com
+ * @Date: 2025-02-17 10:05:11
+ * @LastEditors: wdl
+ * @LastEditTime: 2025-02-17 16:26:12
+ * @Description: 
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>充值总金额：{{ money }}</span>
+      <span>抽奖钥匙：{{ key }}</span>
+      <span>钥匙碎片：{{ shard }}</span>
+      <span>获得的宝箱：{{ box }}</span>
+      <span>获得的积分：{{ score }}</span>
+    </div>
+
+    <div>
+      <button @click="addkey(1)">充值十块</button>
+      <button @click="addkey(11)">充值一百</button>
+      <button @click="oneClick">抽奖一次</button>
+      <button @click="tenClick">抽奖十次</button>
+      <button @click="exchange" :disabled="isDisabled">宝箱兑换钥匙碎片</button>
+      <button @click="shardChange" :disabled="isDisabled">
+        钥匙碎片兑换抽奖钥匙
+      </button>
+    </div>
+
+    <div v-if="!rewardList.length">获得的奖励：0</div>
+    <div v-else>
+      获得的奖励：<span
+        v-for="(item, index) in rewardList"
+        :key="index"
+        @click="salvage(item, index)"
+        >{{ item.title }}</span
+      >
+    </div>
+    <div></div>
+    <div></div>
+    <div class="curbox">
+      <div
+        v-for="(item, index) in curRewardList"
+        :key="index"
+        :class="item.type === 1 ? 'goods' : ''"
+      >
+        {{ `${item.title}(${item.min}-${item.max})-${item.random}` }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// active20250205
+// active20250211
+import { active20250205 } from "./config";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      money: 0,
+      count: 0,
+      key: 0,
+      box: 0,
+      shard: 0,
+      rewardList: [],
+      curRewardList: [],
+      score: 0,
+      active: [],
+      isDisabled: false,
+    };
+  },
+  mounted() {
+    this.initActive(active20250205);
+  },
+  methods: {
+    initActive(list) {
+      let stemp = 0;
+      this.active = list.map((item) => {
+        const newItem = {
+          ...item,
+          min: stemp,
+          max: stemp + item.rate * 100,
+        };
+        if (item.isDisabled) {
+          this.isDisabled = true;
+        }
+        stemp = stemp + item.rate * 100;
+        return newItem;
+      });
+      this.$nextTick(() => {
+        const item = this.active[this.active.length - 1];
+        if (item.max !== 10000) {
+          alert("活动概率配置错误！");
+        }
+      });
+    },
+    exchange() {
+      if (this.box < 1) {
+        alert("宝箱数量不足");
+        return;
+      }
+      this.box -= 1;
+      this.shard += 3;
+    },
+    shardChange() {
+      if (this.shard < 10) {
+        alert("钥匙碎片数量不足");
+        return;
+      }
+      this.shard -= 10;
+      this.key++;
+    },
+    salvage(item, index) {
+      if (item.type !== 1) {
+        return;
+      }
+      const newList = this.rewardList.filter((re, i) => i !== index);
+      this.rewardList = newList;
+      if (item.countType === "score") {
+        this.score += item.count;
+      } else {
+        this.key += item.count;
+      }
+    },
+    addkey(num) {
+      if (num === 1) {
+        this.money += 10;
+      } else {
+        this.money += 100;
+      }
+      this.key += num;
+    },
+    getReward() {
+      const random = Math.ceil(Math.random() * 10000);
+      const curItem = this.active.filter((item) => {
+        return item.min < random && item.max >= random;
+      })[0];
+      if (curItem.type === 1) {
+        this.rewardList = [...this.rewardList, curItem];
+      } else if (curItem.type === 2) {
+        this.box++;
+      } else {
+        this.score += curItem.value;
+      }
+      curItem.random = random;
+      this.curRewardList = [...this.curRewardList, curItem];
+    },
+    oneClick() {
+      if (this.key < 1) {
+        alert("抽奖钥匙不足");
+        return;
+      }
+      this.curRewardList = [];
+      this.key--;
+      this.getReward();
+    },
+    tenClick() {
+      if (this.key < 10) {
+        alert("抽奖钥匙不足");
+        return;
+      }
+      this.curRewardList = [];
+      this.key -= 10;
+      for (let i = 0; i < 10; i++) {
+        this.getReward();
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +178,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .curbox {
+    border-top: 1px solid greenyellow;
+  }
+  span,
+  button {
+    display: inline-block;
+    margin: 0 5px;
+  }
+  div {
+    margin: 5px 0;
+  }
+  .goods {
+    color: red;
+  }
 }
 </style>
---
+++ src/config.js
@@ -0,0 +1,321 @@
+/*
+ * @Author: wdl
+ * @Email: wangdelei4406@yundasys.com
+ * @Date: 2025-02-17 11:13:06
+ * @LastEditors: wdl
+ * @LastEditTime: 2025-02-17 16:12:52
+ * @Description:
+ */
+// 星神活动
+export const active20250205 = [
+  {
+    title: "kar98k-星神", // 道具名称
+    value: "kar98k-星神",
+    rate: 0.2, // 获取概率
+    count: 12, // 分解数量
+    type: 1, // 道具类型： 1 奖励道具 2 宝箱道具 3 积分道具
+  },
+  {
+    title: "幻神-星耀皮肤",
+    value: "幻神-星耀皮肤",
+    rate: 0.25,
+    count: 10,
+    type: 1,
+  },
+  {
+    title: "星际战将",
+    value: "星际战将",
+    rate: 0.25,
+    count: 10,
+    type: 1,
+  },
+  {
+    title: "kar98k-星神音效卡",
+    value: "kar98k-星神音效卡",
+    rate: 0.3,
+    count: 8,
+    type: 1,
+  },
+  {
+    title: "毁灭-星耀皮肤",
+    value: "毁灭-星耀皮肤",
+    rate: 0.3,
+    count: 8,
+    type: 1,
+  },
+  {
+    title: "屠龙-星耀皮肤",
+    value: "屠龙-星耀皮肤",
+    rate: 0.4,
+    count: 6,
+    type: 1,
+  },
+  {
+    title: "an94-超新星",
+    value: "an94-超新星",
+    rate: 0.7,
+    count: 3,
+    type: 1,
+  },
+  {
+    title: "ak12-天启",
+    value: "ak12-天启",
+    rate: 0.8,
+    count: 3,
+    type: 1,
+  },
+  {
+    title: "炼狱-星空",
+    value: "炼狱-星空",
+    rate: 1,
+    count: 2,
+    type: 1,
+  },
+  {
+    title: "羊驼玩偶",
+    value: "羊驼玩偶",
+    rate: 1.2,
+    count: 1,
+    type: 1,
+  },
+  {
+    title: "星耀宝箱",
+    value: "星耀宝箱",
+    rate: 7.4,
+    count: 0,
+    type: 2,
+  },
+  {
+    title: "星辰币*100",
+    value: 100,
+    rate: 0.2,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "星辰币*15",
+    value: 15,
+    rate: 2,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "星辰币*9",
+    value: 9,
+    rate: 10,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "星辰币*8",
+    value: 8,
+    rate: 35,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "星辰币*7",
+    value: 7,
+    rate: 40,
+    count: 0,
+    type: 3,
+  },
+];
+// 双传说活动
+export const active20250211 = [
+  {
+    title: "传说辉光-无序列号版",
+    rate: 0.1,
+    count: 40,
+    type: 1,
+  },
+  {
+    title: "传说夜影-无序列号版",
+    rate: 0.1,
+    count: 40,
+    type: 1,
+  },
+  {
+    title: "王者星光",
+    rate: 0.25,
+    count: 12,
+    type: 1,
+  },
+  {
+    title: "关小雨",
+    rate: 0.25,
+    count: 10,
+    type: 1,
+  },
+  {
+    title: "qbz03-金色蔷薇音效卡",
+    rate: 0.3,
+    count: 8,
+    type: 1,
+  },
+  {
+    title: "裁决-流光秘银皮肤",
+    rate: 0.5,
+    count: 5,
+    type: 1,
+  },
+  {
+    title: "COP-旗袍",
+    rate: 0.5,
+    count: 5,
+    type: 1,
+  },
+  {
+    title: "awm-裁决",
+    rate: 0.7,
+    count: 3,
+    type: 1,
+  },
+  {
+    title: "屠龙",
+    rate: 1.0,
+    count: 2,
+    type: 1,
+  },
+  {
+    title: "天使灵狐玩偶",
+    rate: 1.2,
+    count: 1,
+    type: 1,
+  },
+  {
+    title: "传说宝箱",
+    rate: 5.0,
+    type: 2,
+    isDisabled: true,
+  },
+  {
+    title: "传说币*100",
+    value: 100,
+    rate: 0.1,
+    type: 3,
+  },
+  {
+    title: "传说币*20",
+    value: 20,
+    rate: 1,
+    type: 3,
+  },
+  {
+    title: "传说币*10",
+    value: 10,
+    rate: 9,
+    type: 3,
+  },
+  {
+    title: "传说币*9",
+    value: 9,
+    rate: 40,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "传说币*8",
+    value: 8,
+    rate: 40,
+    count: 0,
+    type: 3,
+  },
+];
+
+export const active20250125 = [
+  {
+    title: "司雨",
+    rate: 0.15,
+    count: 90,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "星神-傲竹金蛇",
+    rate: 0.15,
+    count: 90,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "幻神-纸灯笼皮肤",
+    rate: 0.15,
+    count: 90,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "火麒麟-傲竹金蛇",
+    rate: 0.2,
+    count: 72,
+    type: 1,
+  },
+  {
+    title: "金色蔷薇-纸灯笼皮肤",
+    rate: 0.2,
+    count: 72,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "擎天-傲竹金蛇",
+    rate: 0.3,
+    count: 55,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "龙啸-纸灯笼皮肤",
+    rate: 0.4,
+    count: 30,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "云螭玩偶",
+    rate: 1.0,
+    count: 9,
+    countType: "score",
+    type: 1,
+  },
+  {
+    title: "代金券",
+    rate: 14.35,
+    count: 1,
+    type: 2,
+    isDisabled: true,
+  },
+  {
+    title: "积分*100",
+    value: 100,
+    rate: 0.1,
+    type: 3,
+  },
+  {
+    title: "积分*10",
+    value: 10,
+    rate: 1,
+    type: 3,
+  },
+  {
+    title: "积分*5",
+    value: 5,
+    rate: 2,
+    type: 3,
+  },
+  {
+    title: "积分*4",
+    value: 4,
+    rate: 30,
+    count: 0,
+    type: 3,
+  },
+  {
+    title: "积分*3",
+    value: 3,
+    rate: 50,
+    count: 0,
+    type: 3,
+  },
+];
